9331886f2ea6bd63508aa00aadb41c9e75194088
LUIS GABRIEL ACEVEDO HERNANDEZ <acebedohernandez1@gmail.com>
2025-03-26 23:08:52 -0600

    commit controller

--- backen/controller/greet.controller.js
+++ backen/controller/greet.controller.js
@@ -1,8 +1,11 @@
-const {saludar} = require("../service/greet.service");
+const {saludar} = require("../service/greet.service")
+
+
+
 
 const saludarController = (req,res)=>{
-    const mensajSaludo = saludar();
-    res.json(mensajSaludo);
+    const mensajSaludo = saludar()
+    res.json(mensajSaludo)
 }
 
 module.exports = {saludarController}
