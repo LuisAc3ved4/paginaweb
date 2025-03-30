6c29f12efb19789ca78d032a316368132ec57617
LUIS GABRIEL ACEVEDO HERNANDEZ <acebedohernandez1@gmail.com>
2025-03-29 23:24:19 -0600

    commit

--- backen/routes/greet.routes.js
+++ backen/routes/greet.routes.js
@@ -1,19 +1,10 @@
-const { saludarController} = require("../controller/greet.controller");
-
+const { saludarController } = require("../controller/greet.controller");
+const { ProductController } = require("../controller/products.controller");
 const router = require("express").Router();
-//const routerb = require("express").Router();
-
-router.get("/", saludarController);      //*Endpoints
-//router.get("/bye", despedirController);
 
 
+router.get("/", saludarController)
+router.get("/saludo-2",saludarController)
 
-//funcion callback
+
 module.exports = router
-//module.exports = routerb
-
-
-//const {despedirController} = require("../controller/greet.controller");
-//const routerb = require("express").Router();
-//routerb.get("/",despedirController);
-//module.exports = routerb
